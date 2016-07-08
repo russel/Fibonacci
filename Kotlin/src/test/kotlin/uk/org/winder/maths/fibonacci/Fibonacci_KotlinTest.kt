ca6d76bfc5a4f4d1af60c2d292c561dce00af477
Russel Winder <russel@winder.org.uk>
2016-07-08 15:19:48 +0100

    Add KotlinTests for the main functions.

---
+++ Kotlin/src/test/kotlin/uk/org/winder/maths/fibonacci/Fibonacci_KotlinTest.kt
@@ -0,0 +1,47 @@
+package uk.org.winder.maths.fibonacci
+
+import io.kotlintest.specs.StringSpec
+
+class Fibonacci_KotlinTest: StringSpec() {
+  init {
+
+    val algorithms = listOf(
+        "iterative" to { x: Int -> iterative(x) },
+        "naïveRecursive" to { x: Int -> naïveRecursive(x) },
+        "tailRecursive" to { x: Int -> tailRecursive(x) },
+        "sequence" to ::sequence,
+        "foldive" to { x: Int -> foldive(x) }
+    )
+
+    algorithms.forEach{a ->
+      val name = a.first
+      val f = a.second
+
+      (name + ": zeroth Fibonacci Number is 0") { f(0) == zero }
+
+      (name + ": first Fibonacci Number is 1") { f(1) == one }
+
+      (name + ": non-negative arguments obey the recurrence relation") {
+        forAll{i: Int ->
+          if (1 < i && i < 100000) { f(i) == f(i - 1) + f(i - 2) }
+          else { true }
+        }
+      }
+
+      (name + ": negative argument causes exception") {
+        // See https://github.com/kotlintest/kotlintest/issues/70
+        listOf(-100, -20, -2, -1).forEach {
+          shouldThrow<IllegalArgumentException> { f(it) }
+        }
+        /*
+        forAll{i:Int ->
+          if (i < 0) { shouldThrow<IllegalArgumentException>{ f(i) } }
+          else { true }
+        }
+        */
+      }
+
+    }
+
+  }
+}
