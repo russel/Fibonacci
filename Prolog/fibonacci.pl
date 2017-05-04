d02d3cd9a141605e10a977f9f13ca0879bb213f6
Russel Winder <russel@winder.org.uk>
2017-05-04 08:49:16 +0100

    Add a prolog version.

---
+++ Prolog/fibonacci.pl
@@ -0,0 +1,12 @@
+# -*- mode: prolog; -*-
+
+fibonacci(0, 0).
+fibonacci(1, 1).
+fibonacci(N, R) :- fibonacci(N, R, 0, 1).
+
+fibonacci(0, R, _, R) :- !.
+fibonacci(N, R, P1, P2):-
+   N > 0,
+   NN is N - 1,
+   NP2 is P1 + P2,
+   fibonacci(NN, R, P2, NP2).
