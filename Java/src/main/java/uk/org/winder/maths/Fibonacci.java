d3fe608558bd016c7e142c45e0fee6a13f913fe0
Russel Winder <russel@winder.org.uk>
2018-03-23 12:05:11 +0000

    Update to JDK10.

--- Java/src/main/java/uk/org/winder/maths/Fibonacci.java
+++ Java/src/main/java/uk/org/winder/maths/Fibonacci.java
@@ -9,89 +9,115 @@ import java.util.Map;
 import static java.lang.Math.sqrt;
 
 public class Fibonacci {
-  private static final BigInteger zero = BigInteger.ZERO;
-  private static final BigInteger one = BigInteger.ONE;
-  private static final BigInteger two = one.add(one);
+    private static final BigInteger zero = BigInteger.ZERO;
+    private static final BigInteger one = BigInteger.ONE;
+    private static final BigInteger two = one.add(one);
 
-  private static void validate(final BigInteger n) {
-    if (n.compareTo(zero) < 0) { throw new IllegalArgumentException("index must be positive."); }
-  }
-
-  public static BigInteger iterative(final long n) { return iterative(BigInteger.valueOf(n)); }
-  public static BigInteger iterative(final BigInteger n) {
-    validate(n);
-    BigInteger result = zero;
-    BigInteger next = one;
-    for (BigInteger i = zero; i.compareTo(n) < 0; i = i.add(one)) {
-      BigInteger temporary = result;
-      result = next;
-      next = temporary.add(next);
-    }
-    return result;
-  }
-
-  public static BigInteger naïveRecursive(final  long n) { return naïveRecursive(BigInteger.valueOf(n)); }
-  public static BigInteger naïveRecursive(final  BigInteger n) {
-    validate(n);
-    return
-      n.compareTo(zero) == 0 ? zero :
-      n.compareTo(one) == 0 ? one : naïveRecursive(n.subtract(one)).add(naïveRecursive(n.subtract(two)));
-  }
-
-  static Map<BigInteger,BigInteger> memo = new HashMap<BigInteger,BigInteger>();
-  static {
-    memo.put(zero, zero);
-    memo.put(one, one);
-  }
-
-  public static BigInteger memoizedRecursive(final long n) { return memoizedRecursive(BigInteger.valueOf(n)); }
-  public static BigInteger memoizedRecursive(final BigInteger n) {
-    validate(n);
-    if(! memo.containsKey(n)) {
-      memo.put(n, memoizedRecursive(n.subtract(one)).add(memoizedRecursive(n.subtract(two))));
-    }
-    return memo.get(n);
-  }
-
-  private static final BigDecimal bd_one = BigDecimal.ONE;
-  private static final BigDecimal bd_two = bd_one.add(bd_one);
-  private static final BigDecimal bd_half = BigDecimal.valueOf(0.5);
-
-  public static BigInteger closedForm(final long n) { return closedForm(BigInteger.valueOf(n)); }
-  public static BigInteger closedForm(final BigInteger n) {
-    validate(n);
-    final BigDecimal sqrt5 = BigDecimal.valueOf(sqrt(5));
-    return sqrt5
-        .add(bd_one)
-        .divide(bd_two)
-        .pow(n.intValue())
-        .divide(sqrt5, BigDecimal.ROUND_HALF_DOWN)
-        .add(bd_half)
-        .toBigInteger();
-  }
-
-  public static class Sequence {
-    private final static ArrayList<BigInteger> numbers = new ArrayList<>();
-    static {
-      numbers.add(zero);
-      numbers.add(one);
-    }
-    public static class Iterator implements java.util.Iterator<BigInteger> {
-      private int currentIndex = 0;
-      @Override public boolean hasNext() { return true; }
-      @Override public BigInteger next() { return Sequence.getAt(currentIndex++); }
-      @Override public void remove() { throw new UnsupportedOperationException(); }
-    }
-    public static Iterator iterator() { return new Iterator(); }
-    public static BigInteger getAt(final int index) {
-      if (index < 0) { throw new IllegalArgumentException("Negative index not allowed."); }
-      if (index >= numbers.size()) {
-        numbers.ensureCapacity(index + 1);
-        for (int i = numbers.size() ; i <= index; ++i) {
-          numbers.add(numbers.get(i - 1).add(numbers.get(i - 2)));
+    private static void validate(final BigInteger n) {
+        if (n.compareTo(zero) < 0) {
+            throw new IllegalArgumentException("index must be positive.");
         }
-      }
-      return numbers.get(index);
     }
-  }
+
+    public static BigInteger iterative(final long n) {
+        return iterative(BigInteger.valueOf(n));
+    }
+
+    public static BigInteger iterative(final BigInteger n) {
+        validate(n);
+        var result = zero;
+        var next = one;
+        for (var i = zero; i.compareTo(n) < 0; i = i.add(one)) {
+            var temporary = result;
+            result = next;
+            next = temporary.add(next);
+        }
+        return result;
+    }
+
+    public static BigInteger naïveRecursive(final long n) {
+        return naïveRecursive(BigInteger.valueOf(n));
+    }
+
+    public static BigInteger naïveRecursive(final BigInteger n) {
+        validate(n);
+        return
+                n.compareTo(zero) == 0 ? zero :
+                        n.compareTo(one) == 0 ? one :
+                                naïveRecursive(n.subtract(one)).add(naïveRecursive(n.subtract(two)));
+    }
+
+    private static Map<BigInteger, BigInteger> memo = new HashMap<BigInteger, BigInteger>();
+    static {
+        memo.put(zero, zero);
+        memo.put(one, one);
+    }
+
+    public static BigInteger memoizedRecursive(final long n) {
+        return memoizedRecursive(BigInteger.valueOf(n));
+    }
+
+    public static BigInteger memoizedRecursive(final BigInteger n) {
+        validate(n);
+        if (!memo.containsKey(n)) {
+            memo.put(n, memoizedRecursive(n.subtract(one)).add(memoizedRecursive(n.subtract(two))));
+        }
+        return memo.get(n);
+    }
+
+    private static final BigDecimal bd_one = BigDecimal.ONE;
+    private static final BigDecimal bd_two = bd_one.add(bd_one);
+    private static final BigDecimal bd_half = BigDecimal.valueOf(0.5);
+
+    public static BigInteger closedForm(final long n) {
+        return closedForm(BigInteger.valueOf(n));
+    }
+
+    public static BigInteger closedForm(final BigInteger n) {
+        validate(n);
+        final var sqrt5 = BigDecimal.valueOf(sqrt(5));
+        return sqrt5
+                .add(bd_one)
+                .divide(bd_two)
+                .pow(n.intValue())
+                .divide(sqrt5, BigDecimal.ROUND_HALF_DOWN)
+                .add(bd_half)
+                .toBigInteger();
+    }
+
+    public static class Sequence {
+        private final static ArrayList<BigInteger> numbers = new ArrayList<BigInteger>();
+        static {
+            numbers.add(zero);
+            numbers.add(one);
+        }
+
+        public static class Iterator implements java.util.Iterator<BigInteger> {
+            private int currentIndex = 0;
+
+            @Override
+            public boolean hasNext() { return true; }
+
+            @Override
+            public BigInteger next() { return Sequence.getAt(currentIndex++); }
+
+            @Override
+            public void remove() { throw new UnsupportedOperationException(); }
+        }
+
+        public static Iterator iterator() { return new Iterator(); }
+
+        public static BigInteger getAt(final int index) {
+            if (index < 0) {
+                throw new IllegalArgumentException("Negative index not allowed.");
+            }
+            if (index >= numbers.size()) {
+                numbers.ensureCapacity(index + 1);
+                for (var i = numbers.size(); i <= index; ++i) {
+                    numbers.add(numbers.get(i - 1).add(numbers.get(i - 2)));
+                }
+            }
+            return numbers.get(index);
+        }
+    }
 }
--- Java/src/test/java/uk/org/winder/maths/Fibonacci_TestNG.java
+++ Java/src/test/java/uk/org/winder/maths/Fibonacci_TestNG.java
@@ -2,7 +2,6 @@ package uk.org.winder.maths;
 
 import java.math.BigInteger;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.function.LongFunction;
 
 import org.testng.annotations.Test;
@@ -11,104 +10,106 @@ import org.testng.annotations.DataProvider;
 import static org.testng.Assert.assertEquals;
 
 public class Fibonacci_TestNG {
-  private Object[][] positiveData = new Object[][] {
-      {0L, BigInteger.valueOf(0)},
-      {1L, BigInteger.valueOf(1)},
-      {2L, BigInteger.valueOf(1)},
-      {3L, BigInteger.valueOf(2)},
-      {4L, BigInteger.valueOf(3)},
-      {5L, BigInteger.valueOf(5)},
-      {6L, BigInteger.valueOf(8)},
-      {7L, BigInteger.valueOf(13)},
-      {8L, BigInteger.valueOf(21)},
-      {9L, BigInteger.valueOf(34)},
-      {10L, BigInteger.valueOf(55)},
-      {11L, BigInteger.valueOf(89)},
-      {12L, BigInteger.valueOf(144)},
-      {13L, BigInteger.valueOf(233)},
-      {14L, BigInteger.valueOf(377)},
-      {15L, BigInteger.valueOf(610)},
-      {16L, BigInteger.valueOf(987)},
-      {17L, BigInteger.valueOf(1597)},
-      {18L, BigInteger.valueOf(2584)},
-      {19L, BigInteger.valueOf(4181)},
-      {20L, BigInteger.valueOf(6765)},
-  };
+    private Object[][] positiveData = new Object[][]{
+            {0L, BigInteger.valueOf(0)},
+            {1L, BigInteger.valueOf(1)},
+            {2L, BigInteger.valueOf(1)},
+            {3L, BigInteger.valueOf(2)},
+            {4L, BigInteger.valueOf(3)},
+            {5L, BigInteger.valueOf(5)},
+            {6L, BigInteger.valueOf(8)},
+            {7L, BigInteger.valueOf(13)},
+            {8L, BigInteger.valueOf(21)},
+            {9L, BigInteger.valueOf(34)},
+            {10L, BigInteger.valueOf(55)},
+            {11L, BigInteger.valueOf(89)},
+            {12L, BigInteger.valueOf(144)},
+            {13L, BigInteger.valueOf(233)},
+            {14L, BigInteger.valueOf(377)},
+            {15L, BigInteger.valueOf(610)},
+            {16L, BigInteger.valueOf(987)},
+            {17L, BigInteger.valueOf(1597)},
+            {18L, BigInteger.valueOf(2584)},
+            {19L, BigInteger.valueOf(4181)},
+            {20L, BigInteger.valueOf(6765)},
+    };
 
-  private Object[] negativeData = new Object[] {-1L, -2L, -5L, -10L, -20L, -100L};
+    private Object[] negativeData = new Object[]{-1L, -2L, -5L, -10L, -20L, -100L};
 
-  private Object[] algorithms = new Object[] {
-      (LongFunction<BigInteger>)Fibonacci::iterative,
-      (LongFunction<BigInteger>)Fibonacci::naïveRecursive,
-      (LongFunction<BigInteger>)Fibonacci::memoizedRecursive,
-      (LongFunction<BigInteger>)Fibonacci::closedForm,
-  };
+    private Object[] algorithms = new Object[]{
+            (LongFunction<BigInteger>) Fibonacci::iterative,
+            (LongFunction<BigInteger>) Fibonacci::naïveRecursive,
+            (LongFunction<BigInteger>) Fibonacci::memoizedRecursive,
+            (LongFunction<BigInteger>) Fibonacci::closedForm,
+    };
 
-  @DataProvider
-  private Object[][] algorithmsAndPositiveData() {
-    final ArrayList<Object[]> values = new ArrayList<>();
-    for (Object f: algorithms) {
-      for (Object[] items: positiveData) {
-        if (items.length != 2) { throw new RuntimeException("positiveData array borked."); }
-        values.add(new Object[] {f, items[0], items[1]});
-      }
+    @DataProvider
+    private Object[][] algorithmsAndPositiveData() {
+        final var values = new ArrayList<Object[]>();
+        for (var f: algorithms) {
+            for (var items: positiveData) {
+                if (items.length != 2) {
+                    throw new RuntimeException("positiveData array borked.");
+                }
+                values.add(new Object[]{f, items[0], items[1]});
+            }
+        }
+        return values.toArray(new Object[3][0]);
     }
-    return values.toArray(new Object[3][0]);
-  }
 
-  @DataProvider
-  private Object[][] algorithmsAndNegativeData() {
-    final ArrayList<Object[]> values = new ArrayList<>();
-    for (Object f: algorithms) {
-      for (Object item: negativeData) {
-        values.add(new Object[] {f, item});
-      }
+    @DataProvider
+    private Object[][] algorithmsAndNegativeData() {
+        final var values = new ArrayList<Object[]>();
+        for (var f: algorithms) {
+            for (var item: negativeData) {
+                values.add(new Object[]{f, item});
+            }
+        }
+        return values.toArray(new Object[3][0]);
     }
-    return values.toArray(new Object[3][0]);
-  }
 
-  @Test(dataProvider= "algorithmsAndPositiveData")
-  public void positiveArgumentsShouldWork(LongFunction<BigInteger> f, long n, BigInteger expected) {
-    assertEquals(f.apply(n), expected);
-  }
-
-  @Test(dataProvider= "algorithmsAndNegativeData", expectedExceptions={IllegalArgumentException.class})
-  public void negativeArgumentsShouldThrowException(LongFunction<BigInteger> f, long n) {
-    f.apply(n);
-  }
-
-  @Test
-  public void javaStaticAccess() {
-    assertEquals(Fibonacci.Sequence.getAt(7), BigInteger.valueOf(13));
-    assertEquals(Fibonacci.Sequence.getAt(4), BigInteger.valueOf(3));
-    assertEquals(Fibonacci.Sequence.getAt(7), BigInteger.valueOf(13));
-    assertEquals(Fibonacci.Sequence.getAt(12), BigInteger.valueOf(144));
-    assertEquals(Fibonacci.Sequence.getAt(8), BigInteger.valueOf(21));
-  }
-
-  @Test
-  public void javaInstanceAccess() {
-    final Fibonacci.Sequence fs = new Fibonacci.Sequence();
-    assertEquals(fs.getAt(7), BigInteger.valueOf(13));
-    assertEquals(fs.getAt(4), BigInteger.valueOf(3));
-    assertEquals(fs.getAt(7), BigInteger.valueOf(13));
-    assertEquals(fs.getAt(12), BigInteger.valueOf(144));
-    assertEquals(fs.getAt(8), BigInteger.valueOf(21));
-  }
-
-  @Test
-  public void javaIteratorFunction() {
-    final Iterator<BigInteger> fs = Fibonacci.Sequence.iterator();
-    for (int i = 0; i < positiveData.length; ++i) {
-      assertEquals(fs.next(), positiveData[i][1]);
+    @Test(dataProvider = "algorithmsAndPositiveData")
+    public void positiveArgumentsShouldWork(LongFunction<BigInteger> f, long n, BigInteger expected) {
+        assertEquals(f.apply(n), expected);
     }
-  }
 
-  @Test
-  public void javaIteratorClassInstance() {
-    final Iterator<BigInteger> fs = new Fibonacci.Sequence.Iterator();
-    for (int i = 0; i < positiveData.length; ++i) {
-      assertEquals(fs.next(), positiveData[i][1]);
+    @Test(dataProvider = "algorithmsAndNegativeData", expectedExceptions = {IllegalArgumentException.class})
+    public void negativeArgumentsShouldThrowException(LongFunction<BigInteger> f, long n) {
+        f.apply(n);
     }
-  }
+
+    @Test
+    public void javaStaticAccess() {
+        assertEquals(Fibonacci.Sequence.getAt(7), BigInteger.valueOf(13));
+        assertEquals(Fibonacci.Sequence.getAt(4), BigInteger.valueOf(3));
+        assertEquals(Fibonacci.Sequence.getAt(7), BigInteger.valueOf(13));
+        assertEquals(Fibonacci.Sequence.getAt(12), BigInteger.valueOf(144));
+        assertEquals(Fibonacci.Sequence.getAt(8), BigInteger.valueOf(21));
+    }
+
+    @Test
+    public void javaInstanceAccess() {
+        final Fibonacci.Sequence fs = new Fibonacci.Sequence();
+        assertEquals(fs.getAt(7), BigInteger.valueOf(13));
+        assertEquals(fs.getAt(4), BigInteger.valueOf(3));
+        assertEquals(fs.getAt(7), BigInteger.valueOf(13));
+        assertEquals(fs.getAt(12), BigInteger.valueOf(144));
+        assertEquals(fs.getAt(8), BigInteger.valueOf(21));
+    }
+
+    @Test
+    public void javaIteratorFunction() {
+        final var fs = Fibonacci.Sequence.iterator();
+        for (var i = 0; i < positiveData.length; ++i) {
+            assertEquals(fs.next(), positiveData[i][1]);
+        }
+    }
+
+    @Test
+    public void javaIteratorClassInstance() {
+        final var fs = new Fibonacci.Sequence.Iterator();
+        for (var i = 0; i < positiveData.length; ++i) {
+            assertEquals(fs.next(), positiveData[i][1]);
+        }
+    }
 }
