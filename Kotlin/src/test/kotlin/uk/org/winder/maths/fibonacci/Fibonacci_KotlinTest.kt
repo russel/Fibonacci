0a9d9b2e4d0353531b1b41b2802828595acafdde
Russel Winder <russel@winder.org.uk>
2018-05-12 18:00:23 +0100

    Update to Kotlintest 3.1.0: use verifyAll; remove Gradle plugin.

--- Kotlin/src/test/kotlin/uk/org/winder/maths/fibonacci/Fibonacci_KotlinTest.kt
+++ Kotlin/src/test/kotlin/uk/org/winder/maths/fibonacci/Fibonacci_KotlinTest.kt
@@ -6,8 +6,8 @@ import kotlin.coroutines.experimental.buildSequence
 import io.kotlintest.shouldThrow
 import io.kotlintest.specs.StringSpec
 import io.kotlintest.properties.Gen
-import io.kotlintest.properties.forAll
-import io.kotlintest.tables.forAll as tableForAll
+import io.kotlintest.properties.verifyAll
+import io.kotlintest.tables.forAll
 import io.kotlintest.tables.table
 import io.kotlintest.tables.headers
 import io.kotlintest.tables.row
@@ -22,31 +22,31 @@ val smallishWholeNumbers = object: Gen<Int> {
 class Fibonacci_KotlinTest: StringSpec({
 
 	val algorithms = table(
-			headers("name", "f"),
-			row("iterative", {x: Int -> iterative(x)}),
-			// row("naïveRecursive", {x: Int -> naïveRecursive(x)}),  // Exponential time and space behaviour. :-(
-			row("tailRecursive", {x: Int -> tailRecursive(x)}),
-			row("sequence", {x: Int -> sequence(x)}),
-			row("foldive", {x: Int -> foldive(x)})
-			)
+		headers("name", "f"),
+		row("iterative", {x: Int -> iterative(x)}),
+		// row("naïveRecursive", {x: Int -> naïveRecursive(x)}),  // Exponential time and space behaviour. :-(
+		row("tailRecursive", {x: Int -> tailRecursive(x)}),
+		row("sequence", {x: Int -> sequence(x)}),
+		row("foldive", {x: Int -> foldive(x)})
+	)
 
-			tableForAll(algorithms){name, f ->
+	forAll(algorithms){name, f ->
 
-				"$name: zeroth Fibonacci Number is 0" { f(0) == zero }
+		"$name: zeroth Fibonacci Number is 0" { f(0) == zero }
 
-				"$name: first Fibonacci Number is 1" { f(1) == one }
+		"$name: first Fibonacci Number is 1" { f(1) == one }
 
-				"$name: non-negative arguments obey the recurrence relation" {
-					forAll(smallishWholeNumbers){i -> f(i + 2) == f(i + 1) + f(i)}
-				}
+		"$name: non-negative arguments obey the recurrence relation" {
+			verifyAll(smallishWholeNumbers){i -> f(i + 2) == f(i + 1) + f(i)}
+		}
 
-				"$name: negative argument causes exception" {
-					forAll(Gen.negativeIntegers()){i:Int ->
-						shouldThrow<IllegalArgumentException>{f(i)}
-						true
-					}
-				}
-
+		"$name: negative argument causes exception" {
+			verifyAll(Gen.negativeIntegers()){i:Int ->
+				shouldThrow<IllegalArgumentException>{f(i)}
+				true
 			}
+		}
 
-		})
+	}
+
+})
