a9117d60a4bee356d5f7fbbbec391ad8b1329903
Russel Winder <russel@winder.org.uk>
2019-05-04 17:38:16 +0100

    Upgrade Kotlin version. Tidy up the Gradle file.

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -1,7 +1,5 @@
-val kotlinVersion = "1.3.30"
-
 plugins {
-	kotlin("jvm") version("1.3.30")
+	kotlin("jvm") version("1.3.31")
 }
 
 repositories {
@@ -12,9 +10,7 @@ repositories {
 dependencies {
 	compile(kotlin("stdlib"))
 	// Need this for the KotlinTest to do the right thing.
-	testCompile("org.jetbrains.kotlin:kotlin-reflect:$kotlinVersion")
-	// There seems to be a cock up in the Kotlin internal dependencies so we have to do this. :-(
-	testCompile("org.jetbrains.kotlin:kotlin-stdlib-jdk7:$kotlinVersion")
+	testCompile("org.jetbrains.kotlin:kotlin-reflect:1.3.31")
 	testCompile("io.kotlintest:kotlintest-runner-junit5:3.+")
 }
 
